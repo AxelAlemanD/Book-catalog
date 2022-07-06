87b9b43f154072cadbdf320ae69adf8bd7cfe204
AxelAlemanD <axelad38@gmail.com>
2022-07-05 19:02:13 -0500

    add: Author routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(AuthorController::class)->group(function () {
+    Route::get('/authors', 'getAll');
+    Route::get('/authors/{id}', 'find');
+    Route::post('/authors/', 'create');
+    Route::put('/authors/{id}/', 'update');
+    Route::delete('/authors/{id}', 'delete');
 });
